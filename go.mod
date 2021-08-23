8ea06652175bc16607a7fd451cffb3f46c795d61
yly <1050463330@qq.com>
2021-08-23 17:20:10 +0800

    first commit

--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module blog-service
 go 1.16
 
 require (
-	github.com/gin-gonic/gin v1.6.3 // indirect
+	github.com/gin-gonic/gin v1.6.3
 	github.com/go-playground/validator/v10 v10.9.0 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/json-iterator/go v1.1.11 // indirect
--- main.go
+++ main.go
@@ -7,8 +7,10 @@ import (
 
 func main()  {
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context){
-		c.JSON(http.StatusOK, gin.H{"message": "pong"})
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
 	})
-
+	r.Run() // 监听并在 0.0.0.0:8080 上启动服务
 }
